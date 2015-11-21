f578f9df98ef0fb7a97185a4ca6b344fee6d9ba4
Kliment <klimenta@futurebit.com>
2015-11-21 07:06:34 -0500

    Update

--- GemQuiz.rb
+++ GemQuiz.rb
@@ -1,112 +1,34 @@
-########################################################
-# a.grid_remove() - makes it not visible
-# a.grid() - makes it visible again
-# a.destroy - destroy the widget
-########################################################
+
+
 require 'tk'
 require 'tkextlib/tile'
 
-root = TkRoot.new {title "GemQuiz"}
-frame = Tk::Tile::Frame.new(root)
-
-root.resizable(false, false)
-root.minsize(width=800, height=600)
-root.maxsize(width=800, height=600)
+$root = TkRoot.new
+$root.minsize(width=800, height=600)
+$root.maxsize(width=800, height=600)
+$root.resizable(false, false)
 
 TkOption.add '*tearOff', 0
 
-menubar = TkMenu.new(root)
-root['menu'] = menubar
-
+menubar = TkMenu.new($root)
+$root['menu'] = menubar
 file = TkMenu.new(menubar)
 menubar.add :cascade, :menu => file, :label => 'File'
-
 file.add :command, :label => 'Open...', :command => proc{openFile}
 file.add :command, :label => 'Exit', :command => proc{exitProgram}
 
 Tk::Tile::Style.configure('Question.TLabel', {"font" => "helvetica 14", "foreground" => "black"})
-Tk::Tile::Style.configure('QuestionNumber.TLabel', {"font" => "helvetica 24", "foreground" => "red"})
+Tk::Tile::Style.configure('QuestionNumber.TLabel', {"font" => "helvetica 20", "foreground" => "red"})
 Tk::Tile::Style.configure('Correct.TLabel', {"font" => "helvetica 14", "foreground" => "darkgreen"})
 Tk::Tile::Style.configure('Answer.TLabel', {"font" => "helvetica 14", "foreground" => "blue"})
+Tk::Tile::Style.configure('Stats.TLabel', {"font" => "helvetica 10", "foreground" => "black"})
 
-frame.grid :column => 2, :row => 0
 
-$btnAnswer = Tk::Tile::Button.new(root) {text "Answer"; command {answerQuestion()}}
-$btnPrev = Tk::Tile::Button.new(root) {text "< Prev"; command {prevQuestion()}}
-$btnNext = Tk::Tile::Button.new(root) {text "Next >"; command {nextQuestion()}}
-$btnFinish = Tk::Tile::Button.new(root) {text "Finish"; command {finishQuestion()}}
-
-$questionNumberlabel  = Tk::Tile::Label.new(root) {text ""}.grid( :column => 1, :row => 0)
-$questionNumberlabel['style'] = "QuestionNumber.TLabel"
-
-$questionlbl = Tk::Tile::Label.new(frame) {text ""
-wraplength 650
-}.grid( :column => 0, :row => 0, :sticky => 'w' )
-$questionlbl['style'] = "Question.TLabel"
-
-$answerAlbl = Tk::Tile::Label.new(root) {
-  text ""
-  wraplength 650
-}.grid( :column => 2, :row => 1, :sticky => 'w')
-$answerAlbl['style'] = "Answer.TLabel"
-
-$answerBlbl = Tk::Tile::Label.new(root) {
-  text ""
-  wraplength 650
-}.grid( :column => 2, :row => 2, :sticky => 'w')
-$answerBlbl['style'] = "Answer.TLabel"
-
-$answerClbl = Tk::Tile::Label.new(root) {
-  text ""
-  wraplength 650
-}.grid( :column => 2, :row => 3, :sticky => 'w')
-$answerClbl['style'] = "Answer.TLabel"
-
-$answerDlbl = Tk::Tile::Label.new(root) {
-  text ""
-  wraplength 650
-}.grid( :column => 2, :row => 4, :sticky => 'w')
-$answerDlbl['style'] = "Answer.TLabel"
-
-$option_one = TkVariable.new( 0 ) # put 1 for checked box
-$checkA = Tk::Tile::CheckButton.new(root) {text ""; variable $option_one; onvalue 'A'; command {yourAnswer}}
-$option_two = TkVariable.new( 0 )
-$checkB = Tk::Tile::CheckButton.new(root) {text ""; variable $option_two; onvalue 'B'; command {yourAnswer}}
-$option_three = TkVariable.new( 0 )
-$checkC = Tk::Tile::CheckButton.new(root) {text ""; variable $option_three; onvalue 'C'; command {yourAnswer}}
-$option_four = TkVariable.new( 0 )
-$checkD = Tk::Tile::CheckButton.new(root) {text ""; variable $option_four; onvalue 'D'; command {yourAnswer}}
-$checkA.grid :column => 1, :row => 1
-$checkB.grid :column => 1, :row => 2
-$checkC.grid :column => 1, :row => 3
-$checkD.grid :column => 1, :row => 4
-$checkA.grid_remove()
-$checkB.grid_remove()
-$checkC.grid_remove()
-$checkD.grid_remove()
-
-$radioChoice = TkVariable.new
-$radioA = Tk::Tile::RadioButton.new(root) {text ''; variable $radioChoice; value 'A'; command {yourAnswer()}}
-$radioB = Tk::Tile::RadioButton.new(root) {text ''; variable $radioChoice; value 'B'; command {yourAnswer()}}
-$radioC = Tk::Tile::RadioButton.new(root) {text ''; variable $radioChoice; value 'C'; command {yourAnswer()}}
-$radioD = Tk::Tile::RadioButton.new(root) {text ''; variable $radioChoice; value 'D'; command {yourAnswer()}}
-$radioA.grid :column => 1, :row => 1
-$radioB.grid :column => 1, :row => 2
-$radioC.grid :column => 1, :row => 3
-$radioD.grid :column => 1, :row => 4
-$radioA.grid_remove()
-$radioB.grid_remove()
-$radioC.grid_remove()
-$radioD.grid_remove()
-
-$contentsArray = Array.new
 $questionLinesArray = Array.new
 $answerAArray = Array.new
 $answerBArray = Array.new
 $answerCArray = Array.new
 $answerDArray = Array.new
-$answerEArray = Array.new
-$answerFArray = Array.new
 $questionsArray = Array.new
 $correctAnswersArray = Array.new
 $myAnswersArray = Array.new
@@ -114,19 +36,25 @@ $questionOnScreen = 0
 $totalQuestions = 1
 
 def openFile
+  arrQuizContents = Array.new
   filename = Tk::getOpenFile
-  f = File.open(filename) or die "Unable to open file..."
-  $contentsArray = []
+  begin
+    f = File.open(filename) or die "Unable to open file..."
+  rescue
+    return
+  end
+  arrQuizContents = []
   f.each_line {|line|
-    $contentsArray.push line
+    arrQuizContents.push line
   }
-  ParseFile()
+  f.close
+  ParseFile(arrQuizContents)
   $questionOnScreen = 1
   StartQuiz()
 end
 
-def ParseFile()
-  $contentsArray.each do |line|
+def ParseFile(arr)
+  arr.each do |line|
     if line.match(/^[Qq][0-9]/)
       $questionLinesArray.push line
     elsif line.match(/^[Aa]\./)
@@ -146,41 +74,158 @@ def ParseFile()
   $totalQuestions = $questionLinesArray.length
 end
 
-def showButtonsOnScreen()
-  $btnAnswer.grid :column => 0, :row => 5, :columnspan => 2
-  $btnPrev.grid :column => 2, :row => 5, :sticky => 'w'
-  $btnNext.grid :column => 2, :row => 5, :sticky => 'e'
-  $btnFinish.grid :column => 4, :row => 5, :sticky => 'e'
-end
-
 def StartQuiz()
+  frmQuestion = Tk::Tile::Frame.new($root) {width 760; height 175}
+  frmQuestion.grid :column => 1, :row => 0, :columnspan => 4
+  frmQuestion.grid_propagate(false)
+  frmAnswerA = Tk::Tile::Frame.new($root) {width 760; height 90}
+  frmAnswerA.grid :column => 1, :row => 1, :columnspan => 4
+  frmAnswerA.grid_propagate(false)
+  frmAnswerB = Tk::Tile::Frame.new($root) {width 760; height 90}
+  frmAnswerB.grid :column => 1, :row => 2, :columnspan => 4
+  frmAnswerB.grid_propagate(false)
+  frmAnswerC = Tk::Tile::Frame.new($root) {width 760; height 90}
+  frmAnswerC.grid :column => 1, :row => 3, :columnspan => 4
+  frmAnswerC.grid_propagate(false)
+  frmAnswerD = Tk::Tile::Frame.new($root) {width 760; height 90}
+  frmAnswerD.grid :column => 1, :row => 4, :columnspan => 4
+  frmAnswerD.grid_propagate(false)
+  frmChoiceA = Tk::Tile::Frame.new($root) {width 40; height 90}
+  frmChoiceA.grid :column => 0, :row => 1
+  frmChoiceA.grid_propagate(false)
+  frmChoiceB = Tk::Tile::Frame.new($root) {width 40; height 90}
+  frmChoiceB.grid :column => 0, :row => 2
+  frmChoiceB.grid_propagate(false)
+  frmChoiceC = Tk::Tile::Frame.new($root) {width 40; height 90}
+  frmChoiceC.grid :column => 0, :row => 3
+  frmChoiceC.grid_propagate(false)
+  frmChoiceD = Tk::Tile::Frame.new($root) {width 40; height 90}
+  frmChoiceD.grid :column => 0, :row => 4
+  frmChoiceD.grid_propagate(false)
+  frmNothing = Tk::Tile::Frame.new($root) {width 40; height 65}
+  frmNothing.grid :column => 0, :row => 5, :rowspan => 2
+  frmNothing.grid_propagate(false)
+  frmStats = Tk::Tile::Frame.new($root) {width 760; height 30}
+  frmStats.grid :column => 1, :row => 6, :columnspan => 4
+  frmStats.grid_propagate(false)
+  frmNumber = Tk::Tile::Frame.new($root) {width 40; height 175}
+  frmNumber.grid :column => 0, :row => 0
+  frmNumber.grid_propagate(false)
+  frmbtnAnswer = Tk::Tile::Frame.new($root) {width 190; height 35}
+  frmbtnAnswer.grid :column => 1, :row => 5
+  frmbtnAnswer.grid_propagate(false)
+  frmbtnPrev = Tk::Tile::Frame.new($root) {width 190; height 35}
+  frmbtnPrev.grid :column => 2, :row => 5
+  frmbtnPrev.grid_propagate(false)
+  frmbtnNext = Tk::Tile::Frame.new($root) {width 190; height 35}
+  frmbtnNext.grid :column => 3, :row => 5
+  frmbtnNext.grid_propagate(false)
+  frmbtnFinish = Tk::Tile::Frame.new($root) {width 190; height 35}
+  frmbtnFinish.grid :column => 4, :row => 5
+  frmbtnFinish.grid_propagate(false)
+
+  btnAnswer = Tk::Tile::Button.new(frmbtnAnswer) {text "Answer"; command {answerQuestion()}}
+  btnPrev = Tk::Tile::Button.new(frmbtnPrev) {text "< Prev"; command {prevQuestion()}}
+  btnNext = Tk::Tile::Button.new(frmbtnNext) {text "Next >"; command {nextQuestion()}}
+  btnFinish = Tk::Tile::Button.new(frmbtnFinish) {text "Finish"; command {finishQuestion()}}
+
+  $questionNumberlabel  = Tk::Tile::Label.new(frmNumber) {text ""}
+  $questionNumberlabel['style'] = "QuestionNumber.TLabel"
+
+  $lblStats = Tk::Tile::Label.new(frmStats) {text "Program by Kliment ANDREEV - 2015"}
+  $lblStats['style'] = "Stats.TLabel"
+  $lblStats.grid :column =>0 , :row => 0
+
+  $questionlbl = Tk::Tile::Label.new(frmQuestion) {text "" ;wraplength 755 }
+  $questionlbl['style'] = "Question.TLabel"
+
+  $answerAlbl = Tk::Tile::Label.new(frmAnswerA) {
+    text ""
+    wraplength 755
+  }.grid( :column => 0, :row => 0, :sticky => 'w')
+
+  $answerAlbl['style'] = "Answer.TLabel"
+
+  $answerBlbl = Tk::Tile::Label.new(frmAnswerB) {
+    text ""
+    wraplength 755
+  }.grid( :column => 0, :row => 1, :sticky => 'w')
+  $answerBlbl['style'] = "Answer.TLabel"
+
+  $answerClbl = Tk::Tile::Label.new(frmAnswerC) {
+    text ""
+    wraplength 755
+  }.grid( :column => 0, :row => 2, :sticky => 'w')
+  $answerClbl['style'] = "Answer.TLabel"
+
+  $answerDlbl = Tk::Tile::Label.new(frmAnswerD) {
+    text ""
+    wraplength 755
+  }.grid( :column => 0, :row => 3, :sticky => 'w')
+  $answerDlbl['style'] = "Answer.TLabel"
+
+  btnAnswer.grid :column => 0, :row => 0
+  btnPrev.grid :column => 0, :row => 0
+  btnNext.grid :column => 0, :row => 0
+  btnFinish.grid :column => 0, :row => 0
+
+  $option_one = TkVariable.new( 0 ) # put 1 for checked box
+  $checkA = Tk::Tile::CheckButton.new(frmChoiceA) {text ""; variable $option_one; onvalue 'A'; command {yourAnswer}}
+  $option_two = TkVariable.new( 0 )
+  $checkB = Tk::Tile::CheckButton.new(frmChoiceB) {text ""; variable $option_two; onvalue 'B'; command {yourAnswer}}
+  $option_three = TkVariable.new( 0 )
+  $checkC = Tk::Tile::CheckButton.new(frmChoiceC) {text ""; variable $option_three; onvalue 'C'; command {yourAnswer}}
+  $option_four = TkVariable.new( 0 )
+  $checkD = Tk::Tile::CheckButton.new(frmChoiceD) {text ""; variable $option_four; onvalue 'D'; command {yourAnswer}}
+  $checkA.grid :column => 0, :row => 0
+  $checkB.grid :column => 0, :row => 0
+  $checkC.grid :column => 0, :row => 0
+  $checkD.grid :column => 0, :row => 0
+  $checkA.grid_remove()
+  $checkB.grid_remove()
+  $checkC.grid_remove()
+  $checkD.grid_remove()
+
+  $radioChoice = TkVariable.new
+  $radioA = Tk::Tile::RadioButton.new(frmChoiceA) {text ''; variable $radioChoice; value 'A'; command {yourAnswer()}}
+  $radioB = Tk::Tile::RadioButton.new(frmChoiceB) {text ''; variable $radioChoice; value 'B'; command {yourAnswer()}}
+  $radioC = Tk::Tile::RadioButton.new(frmChoiceC) {text ''; variable $radioChoice; value 'C'; command {yourAnswer()}}
+  $radioD = Tk::Tile::RadioButton.new(frmChoiceD) {text ''; variable $radioChoice; value 'D'; command {yourAnswer()}}
+  $radioA.grid :column => 0, :row => 0
+  $radioB.grid :column => 0, :row => 0
+  $radioC.grid :column => 0, :row => 0
+  $radioD.grid :column => 0, :row => 0
+  $radioA.grid_remove()
+  $radioB.grid_remove()
+  $radioC.grid_remove()
+  $radioD.grid_remove()
+
   printQuestionOnScreen()
   printAnswersOnScreen()
   printChoicesOnScreen()
-  showButtonsOnScreen()
 end
 
-def yourAnswer()
+def printQuestionOnScreen()
+  $questionNumberlabel.grid :column => 0, :row => 0
+  $questionNumberlabel['text'] = $questionOnScreen.to_s + "."
+  $questionlbl.grid :column => 1, :row => 0
+  $questionlbl['text'] = $questionsArray[$questionOnScreen - 1]
+end
+
+def printAnswersOnScreen()
+  $answerAlbl['text'] = $answerAArray[$questionOnScreen - 1]
+  $answerBlbl['text'] = $answerBArray[$questionOnScreen - 1]
+  $answerClbl['text'] = $answerCArray[$questionOnScreen - 1]
+  $answerDlbl['text'] = $answerDArray[$questionOnScreen - 1]
+end
+
+def printChoicesOnScreen()
+  $strAnswer = ($correctAnswersArray[$questionOnScreen -  1].strip)
   if $strAnswer.length == 1
-    $myAnswersArray[$questionOnScreen - 1] = $radioChoice
+    SingleChoice(true)
   else
-    $myAnswersArray[$questionOnScreen - 1] = ($option_one.to_s + $option_two.to_s + $option_three.to_s + $option_four.to_s).gsub("0","")
+    SingleChoice(false)
   end
-end
-
-def finishQuestion()
-  puts $myAnswersArray
-  puts "-------------------"
-  puts $correctAnswersArray
-  msgBox = Tk.messageBox(
-      'type'    => "ok",
-      'icon'    => "info",
-      'title'   => "GemQuiz - Stats",
-      'message' => "This is message over three
-levels with enters and stuff
-let's see ho w many lijnes
-"
-  )
 end
 
 def SingleChoice(var)
@@ -205,22 +250,6 @@ def SingleChoice(var)
   end
 end
 
-def resetAnswerLabelColors()
-  $answerAlbl['style'] = "Answer.TLabel"
-  $answerBlbl['style'] = "Answer.TLabel"
-  $answerClbl['style'] = "Answer.TLabel"
-  $answerDlbl['style'] = "Answer.TLabel"
-end
-
-def printChoicesOnScreen()
-  $strAnswer = ($correctAnswersArray[$questionOnScreen - 1].strip)
-  if $strAnswer.length == 1
-    SingleChoice(true)
-  else
-    SingleChoice(false)
-  end
-end
-
 def nextQuestion()
   return if $questionOnScreen == 0 || $questionOnScreen == $totalQuestions
   $questionOnScreen = $questionOnScreen + 1
@@ -230,11 +259,11 @@ def nextQuestion()
   printChoicesOnScreen()
 end
 
-def printAnswersOnScreen()
-  $answerAlbl['text'] = $answerAArray[$questionOnScreen - 1]
-  $answerBlbl['text'] = $answerBArray[$questionOnScreen - 1]
-  $answerClbl['text'] = $answerCArray[$questionOnScreen - 1]
-  $answerDlbl['text'] = $answerDArray[$questionOnScreen - 1]
+def resetAnswerLabelColors()
+  $answerAlbl['style'] = "Answer.TLabel"
+  $answerBlbl['style'] = "Answer.TLabel"
+  $answerClbl['style'] = "Answer.TLabel"
+  $answerDlbl['style'] = "Answer.TLabel"
 end
 
 def prevQuestion()
@@ -299,9 +328,27 @@ def answerQuestion()
   end
 end
 
-def printQuestionOnScreen()
-  $questionNumberlabel['text'] = $questionOnScreen.to_s + "."
-  $questionlbl['text'] = $questionsArray[$questionOnScreen - 1]
+def finishQuestion()
+  puts $myAnswersArray
+  puts "-------------------"
+  puts $correctAnswersArray
+  msgBox = Tk.messageBox(
+      'type'    => "ok",
+      'icon'    => "info",
+      'title'   => "GemQuiz - Stats",
+      'message' => "This is message over three
+levels with enters and stuff
+let's see ho w many lijnes
+"
+  )
+end
+
+def yourAnswer()
+  if $strAnswer.length == 1
+    $myAnswersArray[$questionOnScreen - 1] = $radioChoice
+  else
+    $myAnswersArray[$questionOnScreen - 1] = ($option_one.to_s + $option_two.to_s + $option_three.to_s + $option_four.to_s).gsub("0","")
+  end
 end
 
 def exitProgram
@@ -309,3 +356,5 @@ def exitProgram
 end
 
 Tk.mainloop
+
+ 
